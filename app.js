a5aa0540d6d384b6a274aa332c43b8f85aa93ae2
AlokSolanky <aloksolanky1@gmail.com>
2023-08-21 01:08:16 +0530

    previosly it had some errors but now this is perfect

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post("/password/forgotpassword", async (req, res) => {
     const client = Sib.ApiClient.instance;
     const apiKey = client.authentications["api-key"];
     apiKey.apiKey =
-      "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";
+      "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";
 
     const tranEmailApi = new Sib.TransactionalEmailsApi();
     const sender = {
@@ -72,12 +72,18 @@ app.post("/password/forgotpassword", async (req, res) => {
 
 app.get("/password/resetpassword/:forgotId", async (req, res) => {
   const id = req.params.forgotId;
-  PasswordRequest.findOne({ where: { id } }).then((forgotpasswordrequest) => {
-    if (forgotpasswordrequest) {
-      forgotpasswordrequest.update({ active: false });
-      res.sendFile(path.join(__dirname, ".", "public", "resetPassword.html"));
-    }
-  });
+  PasswordRequest.findOne({ where: { id } })
+    .then((forgotpasswordrequest) => {
+      if (forgotpasswordrequest.isActive) {
+        forgotpasswordrequest.update({ isActive: false });
+        res.sendFile(path.join(__dirname, ".", "public", "resetPassword.html"));
+      } else {
+        res.status(401).json({ success: false, msg: "No reset request found" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ err });
+    });
 });
 app.post("/password/forgotpassword/:forgotId", async (req, res) => {
   let password = req.body.password;
