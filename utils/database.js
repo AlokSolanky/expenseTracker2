fe2c2cb6d4b15b73f5f74360b6c3fb8e0d60a33b
AlokSolanky <aloksolanky1@gmail.com>
2023-08-24 02:41:13 +0530

    expense tracker backend

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const Sequelize = require(`sequelize`);
-const sequelize = new Sequelize(`expense`, `root`, `alok`, {
-  dialect: `mysql`,
-  host: `localhost`,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: `mysql`,
+    host: process.env.DB_HOST,
+  }
+);
 module.exports = sequelize;
